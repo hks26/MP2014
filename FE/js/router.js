b3ed4d58cbef5c8d7dda1caa38e6721266c30e16
Franesco Agosti <fra5@duke.edu>
2014-07-21 15:58:39 -0400

    basic side bar in place

--- FE/js/router.js
+++ FE/js/router.js
@@ -11,6 +11,7 @@ App.IndexRoute = Ember.Route.extend({
 
 App.UserRoute = Ember.Route.extend({
   model: function(params) {
-  	return this.store.find('user', params.user_id);
+  	return this.
+  	//return this.store.find('user', params.user_id);
   }
 })
